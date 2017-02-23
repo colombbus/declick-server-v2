897680b8eaf889930b8804a64470537c0d619906
Yovar <yovar@rougain.com>
2017-02-23 11:48:42 +0100

    fix CreateCircuitNodesTable migration

---
+++ database/migrations/2017_02_23_113853_add_circuit_nodes_circuit_id_foreign_key.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCircuitNodesCircuitIdForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('circuit_nodes', function (Blueprint $table) {
+            $table->integer('circuit_id')->unsigned();
+            $table->foreign('circuit_id')
+                ->references('id')->on('circuit')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('circuit_nodes', function (Blueprint $table) {
+            $table->dropForeign('circuit_nodes_circuit_id_foreign');
+            $table->dropColumn('circuit_id');
+        });
+    }
+}
