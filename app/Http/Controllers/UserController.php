e49ce8cddd9523df27550b739a116fc4820a5462
Yovar <yovar@rougain.com>
2017-04-21 19:25:10 +0200

    paginate user entities

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        return User::all();
+        return User::paginate(14);
     }
 
     public function testUsernameAvailable(Request $request)
